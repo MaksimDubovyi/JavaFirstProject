fd48b1046d31c60f499f123d0dc224fb31231a59
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-08-15 19:36:46 +0300

    Засобами підготовлених запитів створити метод (Java) який приймає два числа     і повертає кількість рядків у БД в яких поле val_int знаходитья у заданому     діапазоні     rowsCountInSegment(100,500) --> 3     ** додати версію, яка виводить ці рядки

--- src/main/java/step/learning/db/DbDemo.java
+++ src/main/java/step/learning/db/DbDemo.java
@@ -3,15 +3,14 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Random;
 import java.util.Scanner;
 
 public class DbDemo {
-
+    private  int rangeFrom=0;
+    private  int rangeTo=0;
+     private  Random random = new Random();
     private String url;
     private String user;
     private String password;
@@ -52,9 +51,9 @@ public class DbDemo {
 
         do {
 
-            int choice= IntCheck("Оберіть операцію (Введіть номер та натисніть Enter): \n1# Додавання об'єктів до БД :\n2# Видалення обєкта по Id :\n3# Пошук обєкта по Id :\n4# Перегляд всіх об'єктів з таблиці jpu121_randoms : \n");
+            int choice= IntCheck("Оберіть операцію (Введіть номер та натисніть Enter): \n1# Додавання об'єктів до БД :\n2# Видалення обєкта по Id :\n3# Пошук обєкта по Id :\n4# Перегляд всіх об'єктів з таблиці jpu121_randoms : \n5# Кількість : \n6# Кількість яка знаходитья у заданому діапазоні :\n");
 
-            if (choice<5&&choice>0)
+            if (choice<7&&choice>0)
             {
                 switch (choice)
                 {
@@ -69,6 +68,12 @@ public class DbDemo {
                         break;
                     case 4:
                         selectObjects();
+                        break;
+                    case 5:
+                        ShowCount();
+                        break;
+                    case 6:
+                        rowsCountInSegment();
                         break;
                 }
                 //break;
@@ -153,15 +158,20 @@ public class DbDemo {
 
         try(Statement statement = this.connection.createStatement()) //ADO.NET : SqlCommand
         {
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                String id = resultSet.getString("id");
-                int val_int = resultSet.getInt("val_int");
-                String val_str = resultSet.getString("val_str");
-                float val_float = resultSet.getFloat("val_float");
-
-                System.out.println("id: " + id + ", val_int: " + val_int + ", val_str: " + val_str + ", val_float: " + val_float);
+            ResultSet res = statement.executeQuery(sql);
+            int i=1;
+            while (res.next()) {
+                System.out.printf(
+                        "%d) id - (%s) val_int - (%d) val_str - (%s) val_float - (%f)  %n",
+                        i,
+                        res.getString("id") ,
+                        res.getInt("val_int"),
+                        res.getString("val_str"),
+                        res.getFloat("val_float")
+                );
+                i++;
             }
+            res.close();
             System.out.println("_________________________________________________________________________________________________________________");
         }
         catch (SQLException ex)
@@ -170,6 +180,164 @@ public class DbDemo {
         }
     }
 
+    /**private void showRandoms() {
+        String sql = "SELECT `id`, `val_int`, `val_str`,`val_float` FROM jpu121_randoms" ;   // ';' у кінці SQL команди не потрібна
+        try( Statement statement = this.connection.createStatement() ) {
+            ResultSet res = statement.executeQuery( sql ) ;  // ADO ~ SqlDataReader
+            // ResultSet res - об'єкт для трансферу даних, що є результатом запиту
+            // Особливість БД - робота з великими даними, що означає відсутність
+            // єдиного результату, та одержання даних рядок-за-рядком (ітерування)
+            // res.next() - одержання нового рядка (якщо є - true, немає - false)
+            while( res.next() ) {
+                System.out.printf(
+                        "%s %d %n",
+                        // дані рядку доступні через get-тери
+                        res.getString( 1 ) ,      // !!! у JDBC відлік починається з 1 !!!
+                        res.getInt( "val_int" )   // за іменем колонки
+                );
+            }
+            res.close() ;
+        }
+        catch( SQLException ex ) {
+            System.err.println( ex.getMessage() ) ;
+        }
+    }**/
+
+    private void insertPrepared(int rowCount)
+    {
+        /**
+             Підготовлені (prepared) запити - можна вважати тимчасовими збережними процедурами
+         (скомпільовані запити, які зберігаються у СУБД)
+             Ідея - запит компілюється і скомпільований код зберігається у СУБД протягом відкритого
+         зєднання. Протягом цього часу запит можна повторити, у т.ч. з іншими параметрами
+         (за що їх також називають параметризованими запитами)
+         **/
+        String sql = "INSERT INTO jpu121_randoms(`id`, `val_int`, `val_str`, `val_float`) VALUES( UUID(), ?, ?, ?)";
+        /**
+         Місці для варіативних даних змінюються на ?, не змінні дані (як то UUID())залишається у
+         запиті. Якщо значення беруться у лапки то знак ?  всеодно без лапок.
+         **/
+        try(PreparedStatement prep = this.connection.prepareStatement(sql))
+        {
+            /**
+             На другому етапі (після підготовки - створення тимчасової процедури)
+             здійснюється заповнення параметрів
+             * **/
+            for (int i =0; i<rowCount; i++) {
+                prep.setInt(1, random.nextInt(1000));
+                prep.setString(2, random.nextInt() + "");
+                prep.setDouble(3, random.nextDouble());
+
+                /**
+                 Трктій етап виконання
+                 **/
+                prep.execute();
+            }
+        }
+        catch (SQLException ex)
+        {
+            System.err.println(ex.getMessage());
+        }
+    }
+private  void ShowCount()
+{
+    try(PreparedStatement prep = this.connection.prepareStatement("SELECT COUNT(id) FROM jpu121_randoms" ))
+    {
+        ResultSet res = prep.executeQuery();
+        res.next();
+        System.out.println("Count" + res.getInt(1));
+        /*        Java                                                  DB
+   prepareStatement                                           proc_tmp() {
+"SELECT COUNT(id) FROM jpu121_randoms"     ---------------->    return SELECT COUNT(id) FROM jpu121_randoms }
+   res = prep.executeQuery()               ----------------> CALL proc_tmp() --> Iterator#123
+       (res==Iterator#123)               <-- Iterator#123 --
+   res.next()                              ---------------->   Iterator#123.getNext() - береться 1й рядок
+                                        <-- noname: 7 ------
+   res.getInt( 1 ) - 7
+ */
+    }
+    catch (SQLException ex)
+    {
+        System.err.println(ex.getMessage());
+    }
+}
+private void rowsCountInSegment()
+{
+
+    do {
+        rangeFrom=IntCheck("Задайте діапазон\nВід:");
+        rangeTo=IntCheck("До:");
+        if(rangeFrom==rangeTo||rangeFrom>rangeTo||rangeFrom<0||rangeTo<0)
+        {
+            System.out.println("Некоректний ввід.");
+        }
+        else
+            break;
+    } while (true);
+
+    try(PreparedStatement prep = this.connection.prepareStatement("SELECT COUNT(*) FROM jpu121_randoms WHERE val_int > ? AND val_int < ?" ))
+    {
+        prep.setInt(1, rangeFrom); // Передаємо значення для першого параметра
+        prep.setInt(2, rangeTo);   // Передаємо значення для другого параметра
+
+        ResultSet res = prep.executeQuery();
+        res.next();
+        System.out.println("Count" + res.getInt(1));
+
+
+        do {
+            int choice= IntCheck("\n1# Логувати:\n2# Вихід :\n");
+            if (choice<7&&choice>0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        ShowRange();
+                        break;
+                    case 2:
+                        useDb();
+                        break;
+                }
+            }
+            else
+            {
+                System.out.println("Некорректний ввід.");
+            }
+        } while (true);
+    }
+    catch (SQLException ex)
+    {
+        System.err.println(ex.getMessage());
+    }
+}
+private  void ShowRange()
+{
+    try(PreparedStatement prep = this.connection.prepareStatement("SELECT `id`, `val_int`, `val_str`,`val_float` FROM jpu121_randoms WHERE val_int > ? AND val_int < ?" ))
+    {
+        prep.setInt(1, rangeFrom); // Передаємо значення для першого параметра
+        prep.setInt(2, rangeTo);   // Передаємо значення для другого параметра
+
+        ResultSet res = prep.executeQuery();
+        int i=1;
+        while (res.next()) {
+            System.out.printf(
+                    "%d) id - (%s) val_int - (%d) val_str - (%s) val_float - (%f)  %n",
+                    i,
+                    res.getString("id") ,
+                    res.getInt("val_int"),
+                    res.getString("val_str"),
+                    res.getFloat("val_float")
+            );
+            i++;
+        }
+        res.close();
+        System.out.println("_________________________________________________________________________________________________________________");
+    }
+    catch (SQLException ex)
+    {
+        System.err.println(ex.getMessage());
+    }
+}
     /**
      * Перевірка на ціле число
      * @param str
@@ -311,26 +479,25 @@ public class DbDemo {
     {
         String countStr="Скільки ви бажаєте додати об'єктів в таблицю? ";
 
-        Random random = new Random();
         int Count= IntCheck(countStr);
-        for (int i=0;i<Count;i++)
-        {
-            float val_float =random.nextInt(101);
-            int val_int=random.nextInt(101);
-            String val_str="val_int: "+val_int+" val_float: "+val_float;
 
-            String sql = "INSERT INTO jpu121_randoms(`id`, `val_int`, `val_str`, `val_float`) VALUES( UUID(), " + val_int + ", '" + val_str + "', " + val_float + ")";
+            this.insertPrepared(Count);
+//            float val_float =random.nextInt(101);
+//            int val_int=random.nextInt(101);
+//            String val_str="val_int: "+val_int+" val_float: "+val_float;
+//
+//            String sql = "INSERT INTO jpu121_randoms(`id`, `val_int`, `val_str`, `val_float`) VALUES( UUID(), " + val_int + ", '" + val_str + "', " + val_float + ")";
+//
+//            try(Statement statement = this.connection.createStatement()) //ADO.NET : SqlCommand
+//            {
+//                statement.executeUpdate(sql); //executeUpdate -для запитів без повернення
+//                System.out.println(sql+"\n_________________________________________________________________________________________________________________");
+//            }
+//            catch (SQLException ex)
+//            {
+//                System.out.println(ex.getMessage());
+//            }
 
-            try(Statement statement = this.connection.createStatement()) //ADO.NET : SqlCommand
-            {
-                statement.executeUpdate(sql); //executeUpdate -для запитів без повернення
-                System.out.println(sql+"\n_________________________________________________________________________________________________________________");
-            }
-            catch (SQLException ex)
-            {
-                System.out.println(ex.getMessage());
-            }
-        }
         System.out.println("Вітання об'єкти додані до бази успішно!");
 
     }
